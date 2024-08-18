1b76598d18ab76a3b2f074e94c2ca9aba6ef8265
Wani Mohsin <wanimohsin015@gmail.com>
2024-08-18 12:43:39 +0530

    added

--- mailer.py
+++ mailer.py
@@ -20,9 +20,8 @@ email.set_content(html.substitute({'Message': message}), 'html')
 with smtplib.SMTP(host='smtp.gmail.com', port=587) as smtp:
     smtp.ehlo()
     smtp.starttls()
-    smtp.login('mohsinwani00@gmail.com', 'npewfrxeebaxveap')
-    #smtp.login (your_email,password/app password)
+    smtp.login ('your_email@gmail.com', 'your_password') #enter your email and password or app_password
     smtp.send_message(email)
-    print('all good boss!')
+    print('Email sent')
 
 
